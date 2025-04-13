f845d2d5595f4003e8801266e79a9a1983377c60
Abdelhafidh ZARROUG <Azarroug@upper-link.com>
2025-04-13 17:08:37 +0100

    tests

--- Controllers/AuthenticationController.cs
+++ Controllers/AuthenticationController.cs
@@ -1,10 +1,10 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 using ToDoListApp.DTO;
 using ToDoListApp.Models;
 
-﻿using AutoMapper;
 namespace ToDoListApp.Controllers;
 
 [ApiController]
