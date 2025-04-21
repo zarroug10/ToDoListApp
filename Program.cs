1b5766639b5bf12b3693773ff0e4c359de3685a9
Abdelhafidh ZARROUG <Azarroug@upper-link.com>
2025-04-21 10:52:19 +0100

    feat:Add cors Policy

--- Program.cs
+++ Program.cs
@@ -11,38 +11,51 @@ using ToDoListApp.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container.  
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle  
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Register application services and repositories
+// Register application services and repositories  
 builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
-// Configure database context
-builder.Services.AddDbContext<DataContext>(opt =>
-    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
-        sql => sql.EnableRetryOnFailure(3, TimeSpan.FromSeconds(5), null))
-        .EnableSensitiveDataLogging(builder.Environment.IsDevelopment()));
 
-// Register application services and repositories (AFTER DbContext is registered)
+
+builder.Services.AddDbContext<DataContext>(opt =>
+   opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
+       sql => sql.EnableRetryOnFailure(3, TimeSpan.FromSeconds(5), null))
+       .EnableSensitiveDataLogging(builder.Environment.IsDevelopment()));
+
 builder.Services.AddApplicationServices();
 
-
-// Configure Identity services for authentication and authorization
+// Configure Identity services for authentication and authorization  
 builder.Services.AddIdentity<AppUser, AppRole>(opt =>
 {
     opt.Password.RequiredLength = 8;
     opt.Password.RequireDigit = false;
     opt.Password.RequireUppercase = false;
 })
-    .AddEntityFrameworkStores<DataContext>()
-    .AddDefaultTokenProviders();
+   .AddEntityFrameworkStores<DataContext>()
+   .AddDefaultTokenProviders();
+
+// Configure CORS  
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("SecurePolicy", policy =>
+    {
+        policy.WithOrigins("https://localhost:4200") 
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .WithExposedHeaders("Authorization")
+              .SetIsOriginAllowedToAllowWildcardSubdomains()
+              .AllowCredentials();
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline.  
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -50,6 +63,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors("SecurePolicy");
 app.UseAuthorization();
 app.MapControllers();
 
